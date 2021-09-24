489388911468e60541dec24cac81872557212c76
Darren Kellenschwiler <d.kellenschwiler@nchain.com>
2021-09-24 18:29:05 +0100

    typos

--- shortener/model.go
+++ shortener/model.go
@@ -3,5 +3,5 @@ package shortener
 type Redirect struct {
 	Code      string `json: "code" bson: "code" msgpack: "code"`
 	URL       string `json: "url" bson: "url" msgpack: "url" validate: "empty=false & format=url"`
-	CreatedAt int64  `json: "created_at" bson: "created_at" nsgpack: "created_at"`
+	CreatedAt int64  `json: "created_at" bson: "created_at" msgpack: "created_at"`
 }
--- shortener/service.go
+++ shortener/service.go
@@ -1,6 +1,5 @@
 package shortener
 
-// Redirect Services can find and store short urls
 type RedirectService interface {
 	Find(code string) (*Redirect, error)
 	Store(redirect *Redirect) error
