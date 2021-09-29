c0cdcf6741ec6d3141d1b2953d4e512e465078ca
Darren Kellenschwiler <d.kellenschwiler@nchain.com>
2021-09-29 23:59:46 +0100

    getting the main.go in order

--- tutorial2/cmd/main.go
+++ tutorial2/cmd/main.go
@@ -4,11 +4,12 @@ import (
 	"log"
 	"os"
 
+	"github.com/sirdeggen/famailiarisation/tutorial2/internal/adapters/app/api"
+	"github.com/sirdeggen/famailiarisation/tutorial2/internal/adapters/core/arithmetic"
+	gRPC "github.com/sirdeggen/famailiarisation/tutorial2/internal/adapters/framework/left/grpc"
 	"github.com/sirdeggen/famailiarisation/tutorial2/internal/adapters/framework/right/db"
 	"github.com/sirdeggen/famailiarisation/tutorial2/internal/ports"
 )
-
-//gRPC "github.com/familiarisation/tutorial2/internal/adapters/framework/left/grpc"
 
 func main() {
 	var err error
@@ -16,6 +17,7 @@ func main() {
 	var dbaseAdapter ports.DbPort
 	var core ports.ArithmeticPort
 	var appAdapter ports.APIPort
+	var gRPCAdapter ports.GRCPPort
 
 	dbaseDriver := os.Getenv("DB_DRIVER")
 	dsName := os.Getenv("DS_NAME")
@@ -24,4 +26,12 @@ func main() {
 		log.Fatalf("Failed to init dbase connection: %v", err)
 	}
 	defer dbaseAdapter.CloseDbConnection()
+
+	core = arithmetic.NewAdapter()
+
+	appAdapter = api.NewAdapter(dbaseAdapter, core)
+
+	gRPCAdapter = gRPC.NewAdapter(appAdapter)
+	gRPCAdapter.Run()
+
 }
