ee3289409ebf944fb7f36ca2a4ceeea18c232f18
Darren Kellenschwiler <d.kellenschwiler@nchain.com>
2021-09-30 12:33:40 +0100

    end to end testing

---
+++ tutorial2/internal/adapters/framework/left/grpc/rcp_test.go
@@ -0,0 +1,148 @@
+package rpc
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/test/bufconn"
+
+	"log"
+	"os"
+
+	"github.com/sirdeggen/famailiarisation/tutorial2/internal/adapters/app/api"
+	"github.com/sirdeggen/famailiarisation/tutorial2/internal/adapters/core/arithmetic"
+	"github.com/sirdeggen/famailiarisation/tutorial2/internal/adapters/framework/left/grpc/pb"
+	"github.com/sirdeggen/famailiarisation/tutorial2/internal/adapters/framework/right/db"
+	"github.com/sirdeggen/famailiarisation/tutorial2/internal/ports"
+	"github.com/stretchr/testify/require"
+)
+
+const bufSize = 1024 * 1024
+
+var lis *bufconn.Listener
+
+func init() {
+	var err error
+	lis = bufconn.Listen(bufSize)
+	grpcServer := grpc.NewServer()
+
+	var dbaseAdapter ports.DbPort
+	var core ports.ArithmeticPort
+	var appAdapter ports.APIPort
+	var gRPCAdapter ports.GRCPPort
+
+	dbaseDriver := os.Getenv("DB_DRIVER")
+	dsName := os.Getenv("DS_NAME")
+	dbaseAdapter, err = db.NewAdapter(dbaseDriver, dsName)
+	if err != nil {
+		log.Fatalf("Failed to init dbase connection: %v", err)
+	}
+	defer dbaseAdapter.CloseDbConnection()
+
+	core = arithmetic.NewAdapter()
+
+	appAdapter = api.NewAdapter(dbaseAdapter, core)
+
+	gRPCAdapter = NewAdapter(appAdapter)
+
+	pb.RegisterArithmeticServiceServer(grpcServer, gRPCAdapter)
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("Test server start error: %v", err)
+		}
+	}()
+}
+
+func bufDialer(context.Context, string) (net.Conn, error) {
+	return lis.Dial()
+}
+
+func getGRPCConnection(ctx context.Context, t *testing.T) *grpc.ClientConn {
+	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), grpc.WithInsecure)
+	if err != nil {
+		t.Fatalf("failed to dial bufnet: %v", err)
+	}
+	return conn
+}
+
+func TestGetAddition(t *testing.T) {
+	ctx := context.Background()
+	conn := getGRPCConnection(ctx, t)
+	defer conn.Close()
+
+	client := pb.NewArithmeticServiceClient(conn)
+
+	params := &pb.OperationParameters{
+		A: 1,
+		B: 1,
+	}
+
+	answer, err := client.GetAddition(ctx, params)
+	if err != nil {
+		t.Fatalf("expected: %v, got: %v", nil, err)
+	}
+
+	require.Equal(t, answer.Value, int32(1))
+}
+
+func TestGetSubtraction(t *testing.T) {
+	ctx := context.Background()
+	conn := getGRPCConnection(ctx, t)
+	defer conn.Close()
+
+	client := pb.NewArithmeticServiceClient(conn)
+
+	params := &pb.OperationParameters{
+		A: 1,
+		B: 1,
+	}
+
+	answer, err := client.GetSubtraction(ctx, params)
+	if err != nil {
+		t.Fatalf("expected: %v, got: %v", nil, err)
+	}
+
+	require.Equal(t, answer.Value, int32(1))
+}
+
+func TestGetMultiplication(t *testing.T) {
+	ctx := context.Background()
+	conn := getGRPCConnection(ctx, t)
+	defer conn.Close()
+
+	client := pb.NewArithmeticServiceClient(conn)
+
+	params := &pb.OperationParameters{
+		A: 1,
+		B: 1,
+	}
+
+	answer, err := client.GetMultiplication(ctx, params)
+	if err != nil {
+		t.Fatalf("expected: %v, got: %v", nil, err)
+	}
+
+	require.Equal(t, answer.Value, int32(1))
+}
+
+func TestGetDivision(t *testing.T) {
+	ctx := context.Background()
+	conn := getGRPCConnection(ctx, t)
+	defer conn.Close()
+
+	client := pb.NewArithmeticServiceClient(conn)
+
+	params := &pb.OperationParameters{
+		A: 1,
+		B: 1,
+	}
+
+	answer, err := client.GetDivision(ctx, params)
+	if err != nil {
+		t.Fatalf("expected: %v, got: %v", nil, err)
+	}
+
+	require.Equal(t, answer.Value, int32(1))
+}
